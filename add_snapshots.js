39a6881b4a5d355bc2fae7b7a56f583be2e952ec
Patrick Mulder <mulder.patrick@gmail.com>
2016-04-24 11:59:14 +0200

    fetch multiple symbols at once

--- add_snapshots.js
+++ add_snapshots.js
@@ -3,33 +3,35 @@ var _ = require('underscore');
 
 // connect with db
 var bookshelf = require('./config');
+var Promise = require('bluebird');
 var Stock = require('./models/stock');
 var Snapshot = require('./models/snapshot');
 
 console.log('fetch data');
-fetchSnapshot('INTC')
+fetchSnapshot(['INTC'])
 
-function fetchSnapshot(symbol) {
+function fetchSnapshot(symbols) {
   var result = yahooFinance.snapshot({
-    symbol: symbol,
+    symbols: symbols,
     fields: ['y', 'd', 'j1', 'j4', 'e', 's6', 'e7', 'v', 's', 'l1', 'n', 's6']},
       function(err, data) {
         console.log('results');
         console.log(err);
         console.log(data);
 
-        return saveSnapshot(symbol, data)
-          .finally(function() {
-            return bookshelf.knex.destroy();
-          });
+        Promise.map(data, function(d) {
+          return saveSnapshot(d)
+        })
+        .finally(function() {
+          return bookshelf.knex.destroy();
+        });
      });
 }
 
 // save data
-function saveSnapshot(symbol, data) {
-  var stock = Stock.collection().fetch({symbol: symbol}).then(function(models) {
-    console.log(models.toJSON());
-    return models.findWhere({symbol: symbol}).get('id');
+function saveSnapshot(data) {
+  var stock = Stock.collection().fetch({symbol: data.symbol}).then(function(models) {
+    return models.findWhere({symbol: data.symbol}).get('id');
   })
   .then(function(id) {
     return Snapshot.forge({stock_id: id,
